92af1d208936af86b1d6dfec7ee1eb1b11f8e518
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-07-14 17:05:34 -0400

    Added Player position

--- sportsapp/src/components/League/League.js
+++ sportsapp/src/components/League/League.js
@@ -21,13 +21,6 @@ const League = ({ team }) => {
   let NL = teamData?.queryResults.row.filter((team) => team.league_full === 'National League')
   let AL = teamData?.queryResults.row.filter((team) => team.league_full === 'American League')
 
-  // let AmericanLeague = team?.team?.queryResults.filter((american) => american.mlb_org === "American League")
-  // let NationalLeague = team?.team?.queryResults.filter((national) => national.name_display_full === 'National League')
-
-  // console.log(AmericanLeague)
-
-  // console.log(team?.team?.queryResults)
-
   let result = toggle ? NL : AL
 
   console.log(result)
@@ -35,22 +28,13 @@ const League = ({ team }) => {
   let AmericanLeague = result?.filter((american) => american.mlb_org === "American League")
   let NationalLeague = result?.filter((national) => national.mlb_org === "National League")
 
-  const fetchTeams = async (team_id) => {
-    const response = await fetch(`${baseURL}/json/named.roster_40.bam?${team_id}`)
-    const data = response.json()
-    console.log(response)
-    return data
-  }
-
-
   return (
     <div className='league'>
       {toggle ? <button className='togglebtn' onClick={triggleToggle}>Toggle to AL</button> : <button className='togglebtn' onClick={triggleToggle}>Toggle to NL</button>}
       {result?.map(({ name_display_full, logo, team_id }) => (
         <Card>
-          {/* {toggle ? <button className='togglebtn' onClick={triggleToggle}>Toggle to AL</button> : <button className='togglebtn' onClick={triggleToggle}>Toggle to NL</button>} */}
           <div style={{ 'textAlign': 'center', 'width': '230px', 'marginLeft': '12px' }}>
-            <h2><a style={{ 'textDecoration': 'none', 'color': 'black' }} href={`/team/${team_id}`}>{name_display_full}</a></h2>
+            {team_id ? <h2><a style={{ 'textDecoration': 'none', 'color': 'black' }} href={`/team/${team_id}`}>{name_display_full}</a></h2> : <h2>{name_display_full}</h2>}
             <img style={{ 'height': '250px', 'width': '250px' }} src={logo} alt='Team Logo' />
           </div>
         </Card>
--- sportsapp/src/components/teamdetails/TeamDetails.js
+++ sportsapp/src/components/teamdetails/TeamDetails.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { baseURL } from '../../api/Api'
 import Card from '../card/Card'
 import { useParams } from 'react-router-dom'
+import teamData from '../../teams.json'
 
 
 const TeamDetails = () => {
@@ -14,11 +15,12 @@ const TeamDetails = () => {
   let result = `${baseURL}/json/named.roster_40.bam?${obj}`
 
   useEffect(() => {
-    const fetchTeams = async (team_id) => {
+    const fetchTeams = async () => {
       try {
         const response = await fetch(result)
         let data = await response.json()
         let teamInfo = data?.roster_40.queryResults.row.map((item) => item)
+        console.log(teamInfo)
         setInfo(teamInfo)
       } catch (error) {
         console.log(error)
@@ -28,17 +30,20 @@ const TeamDetails = () => {
   }, [])
 
 
-
   return (
     <>
-      {info?.map(({ name_display_first_last }) => (
-        <Card>
-          <div style={{ 'textAlign': 'center', 'width': '230px', 'marginLeft': '12px' }}>
-            {name_display_first_last}
-          </div>
-        </Card>
-      ))
-      }
+
+      <div style={{ 'display': 'flex', 'flexWrap': 'wrap', 'justifyContent': 'space-evenly' }}>
+        {info?.map(({ name_display_first_last, player_id, team_name, position_txt }) => (
+          <Card key={player_id} >
+            <div style={{ 'textAlign': 'center', 'width': '230px', 'marginLeft': '12px' }}>
+              {name_display_first_last}
+              {position_txt}
+            </div>
+          </Card>
+        ))
+        }
+      </div>
     </>
   )
 }
