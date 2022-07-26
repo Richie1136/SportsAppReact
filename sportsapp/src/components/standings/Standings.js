ab93387d4f83ec91cd3e9ec17895332d853c3aa7
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-07-26 11:14:54 -0400

    Styling the Standings

--- sportsapp/src/components/standings/Standings.js
+++ sportsapp/src/components/standings/Standings.js
@@ -41,24 +41,53 @@ const Standings = () => {
   let NLC = standings?.filter((status) => status.Division).filter((league) => league.League === 'NL').filter((division) => division.Division === 'Central')
   let NLW = standings?.filter((status) => status.Division).filter((league) => league.League === 'NL').filter((division) => division.Division === 'West')
 
+  let headers = (
+    <>
+      <th>W</th>
+      <th>L</th>
+      <th>PCT</th>
+      <th>GB</th>
+      <th>WCGB</th>
+      <th>L10</th>
+      <th>STRK</th>
+      <th>RS</th>
+      <th>RA</th>
+      <th>DIFF</th>
+      <th>Home</th>
+      <th>Away</th>
+    </>
+  )
+
+  let data = standings?.map(({ Name, Wins, Losses, Percentage, GamesBehind, WildCardGamesBehind, LastTenGamesLosses, LastTenGamesWins, Streak }) => (
+    <tr>
+      <td style={{ 'textAlign': 'center' }}>{Name}</td>
+      <td>{Wins}</td>
+      <td>{Losses}</td>
+      <td>{"." + Percentage.toFixed(3).split(".")[1]}</td>
+      <td style={{ 'textAlign': 'center' }}>{GamesBehind === 0 ? '-' : GamesBehind}</td>
+      <td style={{ 'textAlign': 'right' }}>{WildCardGamesBehind === 0 ? '-' : WildCardGamesBehind > 0 ? WildCardGamesBehind : "+" + Math.abs(WildCardGamesBehind.toString())}</td>
+      <td>{LastTenGamesWins}-{LastTenGamesLosses}</td>
+      <td>{Streak}</td>
+      {/* <td>{RunsScored}</td>
+  <td>{RunsAgainst}</td>
+  <td className={`${RunsScored - RunsAgainst > 0 ? 'Positive' : 'Negative'}`}>{RunsScored - RunsAgainst > 0 ? "+" + (RunsScored - RunsAgainst) : RunsScored - RunsAgainst}</td>
+  <td>{HomeWins}-{HomeLosses}</td>
+  <td>{AwayWins}-{AwayLosses}</td> */}
+    </tr>
+  ))
+
+  // const { Name, Wins, Losses, Percentage, GamesBehind, WildCardGamesBehind, LastTenGamesWins, LastTenGamesLosses, RunsScored, RunsAgainst, Streak, HomeWins, HomeLosses, AwayWins, AwayLosses } = items
+  // console.log(standings)
+
+
+
 
   return (
     <>
-      <table style={{ 'marginTop': '15px', 'position': 'relative', 'float': 'left', 'marginLeft': '20px' }}>
+      <table className='table' style={{ 'marginTop': '15px', 'position': 'relative', 'float': 'left', 'marginLeft': '20px' }}>
         <tbody>
           <th>AL East</th>
-          <th>W</th>
-          <th>L</th>
-          <th>PCT</th>
-          <th>GB</th>
-          <th>WCGB</th>
-          <th>L10</th>
-          <th>STRK</th>
-          <th>RS</th>
-          <th>RA</th>
-          <th>DIFF</th>
-          <th>Home</th>
-          <th>Away</th>
+          {headers}
           {ALE?.map(({ Name, Wins, Losses, Percentage, GamesBehind, WildCardGamesBehind, LastTenGamesWins, LastTenGamesLosses, RunsScored, RunsAgainst, Streak, HomeWins, HomeLosses, AwayWins, AwayLosses }) => (
             <tr>
               <td style={{ 'textAlign': 'center' }}>{Name}</td>
@@ -75,23 +104,13 @@ const Standings = () => {
               <td>{HomeWins}-{HomeLosses}</td>
               <td>{AwayWins}-{AwayLosses}</td>
             </tr>
+            // { data }
           ))}
         </tbody>
         <br />
         <tbody>
           <th>AL Central</th>
-          <th>W</th>
-          <th>L</th>
-          <th>PCT</th>
-          <th>GB</th>
-          <th>WCGB</th>
-          <th>L10</th>
-          <th>STRK</th>
-          <th>RS</th>
-          <th>RA</th>
-          <th>DIFF</th>
-          <th>Home</th>
-          <th>Away</th>
+          {headers}
           {ALC?.map(({ Name, Wins, Losses, Percentage, GamesBehind, WildCardGamesBehind, LastTenGamesWins, LastTenGamesLosses, RunsScored, RunsAgainst, Streak, HomeWins, HomeLosses, AwayWins, AwayLosses }) => (
             <tr>
               <td style={{ 'textAlign': 'center' }}>{Name}</td>
@@ -113,18 +132,7 @@ const Standings = () => {
         <br />
         <tbody>
           <th>AL West</th>
-          <th>W</th>
-          <th>L</th>
-          <th>PCT</th>
-          <th>GB</th>
-          <th>WCGB</th>
-          <th>L10</th>
-          <th>STRK</th>
-          <th>RS</th>
-          <th>RA</th>
-          <th>DIFF</th>
-          <th>Home</th>
-          <th>Away</th>
+          {headers}
           {ALW?.map(({ Name, Wins, Losses, Percentage, GamesBehind, WildCardGamesBehind, LastTenGamesWins, LastTenGamesLosses, RunsScored, RunsAgainst, Streak, HomeWins, HomeLosses, AwayWins, AwayLosses }) => (
             <tr>
               <td style={{ 'textAlign': 'center' }}>{Name}</td>
@@ -145,22 +153,20 @@ const Standings = () => {
         </tbody>
         <br />
       </table>
-      <table style={{ 'float': 'right', 'position': 'relative', 'marginTop': '15px', 'marginRight': '70px' }}>
-        <tbody style={{ 'marginLeft': '20px' }}>
-          <tr>
-            <th>NL East</th>
-            <th>W</th>
-            <th>L</th>
-            <th>PCT</th>
-            <th>GB</th>
-            <th>WCGB</th>
-            <th>L10</th>
-            <th>STRK</th>
-            <th>RS</th>
-            <th>RA</th>
-            <th>DIFF</th>
-            <th>Home</th>
-            <th>Away</th>
+      <table className='table' style={{ 'position': 'relative', 'marginTop': '15px' }}>
+        <colgroup style={{ 'width': '410px', 'height': '272px' }} span='6'></colgroup>
+        <colgroup style={{ 'width': '272px', 'height': '96px' }} span='2'></colgroup>
+        <colgroup style={{ 'width': '272px', 'height': '204px' }} span='4'></colgroup>
+        <colgroup style={{ 'width': '272', 'height': '180px' }} span='3'></colgroup>
+
+        <tbody>
+          <tr className='division-row'>
+            <th className='division-name'>
+              <span>
+                <span className='division-title'>NL East</span>
+              </span>
+            </th>
+            {headers}
           </tr>
         </tbody>
         {NLE?.map(({ Name, Wins, Losses, Percentage, GamesBehind, WildCardGamesBehind, LastTenGamesWins, LastTenGamesLosses, RunsScored, RunsAgainst, Streak, HomeWins, HomeLosses, AwayWins, AwayLosses }) => (
@@ -184,18 +190,8 @@ const Standings = () => {
         <tbody style={{ 'marginLeft': '20px' }}>
           <tr>
             <th>NL Central</th>
-            <th>W</th>
-            <th>L</th>
-            <th>PCT</th>
-            <th>GB</th>
-            <th>WCGB</th>
-            <th>L10</th>
-            <th>STRK</th>
-            <th>RS</th>
-            <th>RA</th>
-            <th>DIFF</th>
-            <th>Home</th>
-            <th>Away</th>
+            {headers}
+
           </tr>
         </tbody>
         {NLC?.map(({ Name, Wins, Losses, Percentage, GamesBehind, WildCardGamesBehind, LastTenGamesWins, LastTenGamesLosses, RunsScored, RunsAgainst, Streak, HomeWins, HomeLosses, AwayWins, AwayLosses }) => (
@@ -219,18 +215,8 @@ const Standings = () => {
         <tbody style={{ 'marginLeft': '20px' }}>
           <tr>
             <th>NL West</th>
-            <th>W</th>
-            <th>L</th>
-            <th>PCT</th>
-            <th>GB</th>
-            <th>WCGB</th>
-            <th>L10</th>
-            <th>STRK</th>
-            <th>RS</th>
-            <th>RA</th>
-            <th>DIFF</th>
-            <th>Home</th>
-            <th>Away</th>
+            {headers}
+
           </tr>
         </tbody>
         {NLW?.map(({ Name, Wins, Losses, Percentage, GamesBehind, WildCardGamesBehind, LastTenGamesWins, LastTenGamesLosses, RunsScored, RunsAgainst, Streak, HomeWins, HomeLosses, AwayWins, AwayLosses }) => (
